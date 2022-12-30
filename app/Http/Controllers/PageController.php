0e98dfd1bae26dc5c2bdb2440aec91feb03b3f23
Александр Мельников <aleksrowing232@gmail.com>
2022-12-30 17:09:38 +0300

    Add files via upload

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+
+class PageController extends Controller
+{
+    public function towers_tour()
+    {
+        return view('site.pages.towers_tour');
+    }
+
+    public function scaffolding()
+    {
+        return view('site.pages.scaffolding');
+    }
+
+    public function snow_removal()
+    {
+        return view('site.pages.snow_removal');
+    }
+
+    public function gallery()
+    {
+        return view('site.pages.gallery');
+    }
+
+    public function gallery_tower()
+    {
+        return view('site.pages.gallery_tower');
+    }
+
+    public function gallery_scaffolding()
+    {
+        return view('site.pages.gallery_scaffolding');
+    }
+
+    public function contacts()
+    {
+        return view('site.pages.contacts');
+    }
+
+    public function posts()
+    {
+        $posts = Post::query()->paginate(12);
+
+        return view('site.pages.posts', compact('posts'));
+    }
+}
