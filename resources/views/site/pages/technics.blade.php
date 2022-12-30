d4bef887fa594048871b51b11b65767a5d55a7ca
Aleksandr232 <aleksrowing232@gmail.com>
2022-12-31 00:43:35 +0300

    texnica

--- resources/views/site/pages/technics.blade.php
+++ resources/views/site/pages/technics.blade.php
@@ -16,12 +16,12 @@
         <div class="towers-tour__card my-container">
             <div class="tt-card__image">
                 <picture>
-                    <source type="image/webp" srcset="{{ asset('frontend/img/tour-towers/balton-1.webp') }}">
-                    <img src="{{ asset('frontend/img/technics/texnica1.jpg') }}" width="670" height="350" alt="">
+                    <source type="image/webp" srcset="{{ asset('frontend/img/technics/texnica1.webp') }}">
+                    <img src="{{ asset('frontend/img/technics/texnica1.jpg') }}" width="670" height="450" alt="">
                 </picture>
             </div>
             <div class="tt-card__content">
-                <h2>Балатон 0,6×1,5<span>м</span></h2>
+               <!--  <h2>Балатон 0,6×1,5<span>м</span></h2>
                 <h3>Передвижная сборно-разборная вышка предназначена для производства монтажных, ремонтных и отделочных работ, как снаружи, так и внутри строений и размещения рабочих и материалов непосредственно в зоне работ</h3>
                 <p>Технические характеристики</p>
                 <ol>
@@ -34,13 +34,13 @@
                 </ol>
                 <div class="tt-cc__link">
                     <a href="{{ asset('frontend/doc/balaton-0,6x1,5.pdf') }}" target="_blank">Технический паспорт →</a>
-                </div>
+                </div> -->
             </div>
         </div>
         <div class="tt-card__with__bg">
             <div class="towers-tour__card my-container">
                 <div class="tt-card__content tt-card__revers">
-                    <h2>Балатон 1×2<span>м</span></h2>
+                    <!-- <h2>Балатон 1×2<span>м</span></h2>
                     <h3>Вышки-туры «Балатон 12» более универсальные. Их можно применять на высоте до 21 метра используя стабилизаторы (входящие в комплект) и при необходимости крепления к стене. При высоте до 13,1 метра вышка-тура является самостоящей и не требует крепления к стене</h3>
                     <p>Технические характеристики</p>
                     <ol>
@@ -54,13 +54,13 @@
                     </ol>
                     <div class="tt-cc__link">
                         <a href="{{ asset('frontend/doc/balaton-1x2.pdf') }}" target="_blank">Технический паспорт →</a>
-                    </div>
+                    </div> -->
                 </div>
 
                 <div class="tt-card__image">
                     <picture>
-                        <source type="image/webp" srcset="{{ asset('frontend/img/tour-towers/balton-2.webp') }}">
-                        <img src="{{ asset('frontend/img/technics/texnica2.jpg') }}" width="670" height="350" alt="">
+                        <source type="image/webp" srcset="{{ asset('frontend/img/technics/texnica2.webp') }}">
+                        <img src="{{ asset('frontend/img/technics/texnica2.jpg') }}" width="670" height="450" alt="">
                     </picture>
                 </div>
 
@@ -69,12 +69,12 @@
         <div class="towers-tour__card my-container">
             <div class="tt-card__image">
                 <picture>
-                    <source type="image/webp" srcset="{{ asset('frontend/img/tour-towers/balton-3.webp') }}">
-                    <img src="{{ asset('frontend/img/technics/texnica3.jpg') }}" width="670" height="350" alt="">
+                    <source type="image/webp" srcset="{{ asset('frontend/img/technics/texnica3.webp') }}">
+                    <img src="{{ asset('frontend/img/technics/texnica3.jpg') }}" width="670" height="450" alt="">
                 </picture>
             </div>
             <div class="tt-card__content">
-                <h2>Балатон 2×2<span>м</span></h2>
+                <!-- <h2>Балатон 2×2<span>м</span></h2>
                 <h3>Вышки-туры «Балатон 20» самые мощные из серии. Их высота может достигать 21,3 метра и главное отличие от остальных вышек - это грузоподъемность на всех высотах 400 кг. Размер рабочей площадки 2х2 метра – это позволяет чувствовать себя более комфортно на любой высоте</h3>
                 <p>Технические характеристики</p>
                 <ol>
@@ -87,11 +87,11 @@
                 </ol>
                 <div class="tt-cc__link">
                     <a href="{{ asset('frontend/doc/balaton-2x2.pdf') }}" target="_blank">Технический паспорт →</a>
-                </div>
+                </div> -->
             </div>
         </div>
     </section>
-    <section id="price-towers__tour">
+    <!-- <section id="price-towers__tour">
         <div class="price-towers__tour my-container">
             <h2 class="ptt-title">Стоимость аренды вышек-туров</h2>
             <p class="ptt-description">Минимальная сумма заказа в размере арендной платы за 7 дней</p>
@@ -107,7 +107,7 @@
                 <a target="_blank" href="https://calcarenda.vercel.app/"><button id='bot' class="btn_calc" >Рассчитать</button></a>
             </div>
         </div>
-    </section>
+    </section> -->
     <!-- include our-products start -->
     <section id="towers-tour__recommendations">
         <div class="our-services__title my-container">
